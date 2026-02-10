24bd71bfb7a2faa281307dbb6360028ca674e04c
nehamalviyaa <nehamalviya819@gmail.com>
2026-02-10 17:24:48 +0530

    latest update

--- UserService/src/main/java/com/info/RedisConfig.java
+++ UserService/src/main/java/com/info/RedisConfig.java
@@ -1,65 +1,33 @@
 package com.info;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.springframework.cache.CacheManager;
+
+
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-
 
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
-	
+    
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return  new LettuceConnectionFactory();
-	}
-	
-	@Bean
-	public RedisTemplate<String , Object> redisTemplate(){
-		RedisTemplate<String , Object> template = new RedisTemplate();
-		template.setConnectionFactory(redisConnectionFactory());
-		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    public RedisCacheManager cacheManager(RedisConnectionFactory factory , tools.jackson.databind.ObjectMapper objectMapper) {
+    	
+		GenericJacksonJsonRedisSerializer serializer = new GenericJacksonJsonRedisSerializer(objectMapper);
 		
-		return template;
-	}
-
-	@Bean
-	public CacheManager cacheManager() {
-
-	    RedisCacheConfiguration defaultConfig =
-	        RedisCacheConfiguration.defaultCacheConfig()
-	            .entryTtl(Duration.ofMinutes(10))
-	            .disableCachingNullValues()
-	            .serializeValuesWith(
-	                RedisSerializationContext.SerializationPair
-	                    .fromSerializer(new GenericJackson2JsonRedisSerializer())
-	            );
-
-	    Map<String, RedisCacheConfiguration> cacheConfigMap = new HashMap<>();
-
-	    cacheConfigMap.put("users", defaultConfig);
-	    cacheConfigMap.put("userById", defaultConfig);
-
-	    return RedisCacheManager.builder(redisConnectionFactory())
-	            .cacheDefaults(defaultConfig)
-	            .withInitialCacheConfigurations(cacheConfigMap)
-	            .build();
-	}
-
-	
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+		
+		
+		return RedisCacheManager.builder(factory).cacheDefaults(config).withCacheConfiguration("userList", config.entryTtl(Duration.ofMinutes(5))
+				).withCacheConfiguration("users", config.entryTtl(Duration.ofMinutes(5))
+						).withCacheConfiguration("userById", config.entryTtl(Duration.ofMinutes(5))).build();
+    }
 }
--- UserService/src/main/java/com/info/service/UserService.java
+++ UserService/src/main/java/com/info/service/UserService.java
@@ -6,9 +6,12 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.info.entity.User;
 import com.info.repo.UserRepository;
+
+
 
 
 @Service
@@ -21,44 +24,60 @@ public class UserService {
 	}
 	
 	//save
+	@Transactional
 	@CacheEvict(value = {"users"}, allEntries = true)
 	public User saveUser(User user) {
 		return userRepo.save(user);
 	}
 	
 	//getAllUsers
-	@Cacheable(value = "users")
+	@Cacheable(value = "userList")
 	public List<User> getAllUsers(){
 		return userRepo.findAll();
 		
 	}
 	
 	//getUserByID
-	@Cacheable(value = "userById" , key="#id")
+	@Cacheable(value = "userById" , key="#id", unless = "#result == null" )
 	public User getUserById(Long id) {
 	   
 	        return userRepo.findById(id).orElse(null);
 	   
 	}
 
-	
 	//updateUser
 	@CachePut(value = "userById" , key = "#id")
-	@CacheEvict(value = {"users"} , allEntries = true)
+	@CacheEvict(value = "users" , allEntries = true , beforeInvocation = true)
 	public User updateUser(Long id , User user) {
 		
 		User exsitingUser = userRepo.findById(id).orElseThrow(()-> new RuntimeException("User not found"));
 		exsitingUser.setName(user.getName());
 		exsitingUser.setEmail(user.getEmail());
-		
 	    return userRepo.save(exsitingUser);
 	}
 	
 	
 	//DeleteUser
-	@CacheEvict(value = {"users" , "userById"} , allEntries = true)
+//	@Caching(evict = {
+//		    @CacheEvict(value = "userById", key = "#id"),
+//		    @CacheEvict(value = "userList", allEntries = true , beforeInvocation = true)
+//		})
+	
+	@CacheEvict(value = "userList", allEntries = true , beforeInvocation = true)
 	public void deleteUser(Long id) {
-     userRepo.deleteById(id);
-     System.out.println("User deleted for ID : "+id);
+     try {
+		userRepo.deleteById(id);
+     }
+     catch(Exception e) {
+    	 e.printStackTrace();
+     }
 	}
 }
+
+
+
+
+
+
+
+
